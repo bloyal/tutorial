266246f0c72dd35d56a74c82b161f1b7b33657b0
Brian Loyal <bloyal@gmail.com>
2014-01-01 20:33:58 -0600

    Done up to 11.2.5

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -19,16 +19,19 @@ describe "Authentication" do
 
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error') }
-
       describe "after visiting another page" do
-        before {click_link "Home"}
-        it {should_not have_selector('div.alert.alert-error')}
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
       end
     end
 
-   describe "with valid information" do
+    describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before { sign_in user }
+      before do
+        fill_in "Email",    with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end
 
       it { should have_title(user.name) }
       it { should have_link('Users',       href: users_path) }
@@ -36,8 +39,8 @@ describe "Authentication" do
       it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
- 
-      describe "followed by a signout" do
+
+      describe "followed by signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
       end
@@ -76,12 +79,23 @@ describe "Authentication" do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
         end
-      
+
         describe "visiting the user index" do
           before { visit users_path }
-	  it { should have_title('Sign in') }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
         end
       end
+
       describe "in the Microposts controller" do
 
         describe "submitting to the create action" do
@@ -91,6 +105,18 @@ describe "Authentication" do
 
         describe "submitting to the destroy action" do
           before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
           specify { expect(response).to redirect_to(signin_path) }
         end
       end
